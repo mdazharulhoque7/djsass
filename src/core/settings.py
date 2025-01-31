117c88762e7473832e6288e97b219b5038067d1b
Md. Azharul Hoque <mdazharulhoque7@gmail.com>
2025-01-31 19:12:57 +0600

    Prepare for railway

--- src/core/settings.py
+++ src/core/settings.py
@@ -30,8 +30,15 @@ SECRET_KEY = 'django-insecure-lbtdhhee(tde)1!e*ue46pd!e-pv%r0rb3_u3x6gn505-2!$ok
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.railway.app'
+]
 
+if DEBUG:
+    ALLOWED_HOSTS += [
+        '127.0.0.1',
+        'localhost'
+    ]
 
 # Application definition
 
--- src/core/views.py
+++ src/core/views.py
@@ -5,12 +5,33 @@ from apps.visits.models import PageVisit
 def home(request, *args, **kwargs):
     total_visit = PageVisit.objects.all().count()
     page_visit = PageVisit.objects.filter(path=request.path).count()
-    # print('Path', request.path)
+    try:
+        parcent = (page_visit * 100.00) / total_visit
+    except:
+        parcent = 0
+    
     PageVisit.objects.create(path=request.path)
     return render(request, "home.html", 
                   {
                       "title":"Home Page",
                       "page_visit":page_visit,
-                      "percent":(page_visit * 100.00) / total_visit,
+                      "percent":parcent,
+                      "total_visit":total_visit
+                      })
+
+def about(request, *args, **kwargs):
+    total_visit = PageVisit.objects.all().count()
+    page_visit = PageVisit.objects.filter(path=request.path).count()
+    try:
+        parcent = (page_visit * 100.00) / total_visit
+    except:
+        parcent = 0
+    
+    PageVisit.objects.create(path=request.path)
+    return render(request, "home.html", 
+                  {
+                      "title":"Home Page",
+                      "page_visit":page_visit,
+                      "percent":parcent,
                       "total_visit":total_visit
                       })
