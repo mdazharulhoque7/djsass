0735f5b32cdd66a13a486f14c72bf5d5e53c164f
Md Azharul Hoque <mdazharulhoque7@gmail.com>
2025-02-05 10:27:29 +0600

    Login View and Template

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from auth import views as authViews
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('login/', authViews.login_view, name='login'),
+    path('admin/', admin.site.urls),
     path('about/', views.about, name='about'),
 ]
